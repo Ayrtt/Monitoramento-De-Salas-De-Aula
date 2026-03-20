0a82cf1568b912d38f81174b0e449ff550a3cb11
Ayrtt <86670408+Ayrtt@users.noreply.github.com>
2026-03-20 13:59:51 -0300

    Add files via upload

---
+++ mongodb/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <Arduino.h>
+
+/*rede
+const char* WIFI_SSID = "Assert-Guest";
+const char* WIFI_PASSWORD= "Gu3st@ssert.";
+const char* serverName = "http://192.168.12.18:5000";*/
+
+/*hotspot pc
+#define WIFI_SSID "DESKTOP-Q32KGC5 2858"
+#define WIFI_PASSWORD "14U8s^26"
+#define serverName "http://192.168.137.1:5000";*/ 
+
+/*hotspot celularhttp://10.205.183.130:5000 */
+const char* WIFI_SSID = "motorola one fusion_1902";
+const char* WIFI_PASSWORD = "testeapi";
+const String serverName = "http://kgjgo-200-129-71-140.a.free.pinggy.link";
+
+
+#endif
