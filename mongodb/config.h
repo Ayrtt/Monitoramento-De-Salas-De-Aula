67d23d03014c7b6b182d8f9f0d3b37acff63acb7
Ayrtt <86670408+Ayrtt@users.noreply.github.com>
2026-04-06 17:29:08 -0300

    Create config.h

---
+++ mongodb/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <Arduino.h>
+
+// --- Configurações do Hardware (Pinos) ---
+#define LED_PIN 27
+#define DHT_PIN 14
+#define DHTTYPE DHT11
+
+#define RELAY_PIN 32
+#define PIR_PIN 34
+
+#define RELAY_ON LOW  
+#define RELAY_OFF HIGH 
+
+// --- Configurações dos delays ---
+#define DHT_READ_DELAY 20000
+#define INACTIVITY_TIMEOUT_MS 5000
+//#define AC_INACTIVITY_TIMEOUT_MS 180000
+
+// --- Configurações do Servidor de Tempo (NTP) ---
+#define NTP_SERVER "pool.ntp.org"
+#define GMT_OFFSET_SEC -10800      // Offset para GMT-3 (Brasil)
+#define DAYLIGHT_OFFSET_SEC 0
+
+#endif
