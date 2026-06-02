dcf589a65815083aeb6dca03b8179edc7681134f
Ayrtt <86670408+Ayrtt@users.noreply.github.com>
2026-06-02 15:27:46 -0300

    Update usuario.js

--- mongodb/front/usuario.js
+++ mongodb/front/usuario.js
@@ -1,8 +1,6 @@
 const form = document.getElementById("form-usuario");
 const mensagem = document.getElementById("mensagem");
-const API_URL = "http://192.168.12.18:5000";
-//"https://monitoramento-de-salas.onrender.com"
-//
+const API_URL = "";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
